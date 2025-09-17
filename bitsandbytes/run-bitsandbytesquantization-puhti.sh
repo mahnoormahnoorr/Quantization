f11edfef197a9b62a391e043f3619ae46a6d2d40
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2025-09-17 13:37:23 +0300

    Update run-bitsandbytesquantization-puhti.sh

--- bitsandbytes/run-bitsandbytesquantization-puhti.sh
+++ bitsandbytes/run-bitsandbytesquantization-puhti.sh
@@ -3,9 +3,12 @@
 #SBATCH --partition=gputest
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
+#SBATCH --gpus-per-task=1
+#SBATCH --gpus-per-node=1
 #SBATCH --mem=80G
 #SBATCH --time=0:30:00
 #SBATCH --gres=gpu:v100:1
+#SBATCH --output=slurm-%j.out
 
 # Load the module
 module purge
