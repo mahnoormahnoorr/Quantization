b93bbb377fd6b93e6955de10237c03d05e7edba2
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2025-09-17 13:30:56 +0300

    Update run-bitsandbytesquantization-lumi.sh

--- bitsandbytes/run-bitsandbytesquantization-lumi.sh
+++ bitsandbytes/run-bitsandbytesquantization-lumi.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name=opt125m
 #SBATCH --account=project_xxxxxxx 
 #SBATCH --partition=dev-g 
 #SBATCH --time=00:30:00
 #SBATCH --ntasks=1
+#SBATCH --mem=60G
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-node=1
 #SBATCH --output=slurm-%j.out
 
 # Load the module
