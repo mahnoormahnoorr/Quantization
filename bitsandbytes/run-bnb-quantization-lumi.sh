14a617ac9aaa108815132b54e0cac8ec03de8bb6
Anni Moisala <anni.moisala@csc.fi>
2025-09-18 13:42:46 +0300

    Update run-bnb-quantization-lumi.sh

--- bitsandbytes/run-bnb-quantization-lumi.sh
+++ bitsandbytes/run-bnb-quantization-lumi.sh
@@ -5,9 +5,10 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=7
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --time=00:30:00
 #SBATCH --output=slurm-%j.out
+#SBATCH --output=slurm-%j.err
 
 # Load the module
 module purge
