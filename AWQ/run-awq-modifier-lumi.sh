1b3dab7c5483af1dcc32a18c2fea217d1c145b94
Anni Moisala <anni.moisala@csc.fi>
2025-09-18 15:41:20 +0300

    Create run-awq-modifier-lumi.sh

---
+++ AWQ/run-awq-modifier-lumi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash                                                                                                                   
+#SBATCH --account=project_xxxxxxxx                                                                                          
+#SBATCH --partition=dev-g                                                                                                     
+#SBATCH --ntasks=1                                                                                                            
+#SBATCH --gpus-per-task=1                                                                                                     
+#SBATCH --cpus-per-task=7                                                                                                     
+#SBATCH --gpus-per-node=1                                                                                                     
+#SBATCH --mem=32G                                                                                                             
+#SBATCH --time=00:30:00                                                                                                       
+#SBATCH --output=slurm-%j.out                                                                                                 
+#SBATCH --error=slurm-%j.err                                                                                                  
+                                                                                                                              
+# Load the module                                                                                                             
+module purge                                                                                                                  
+module use /appl/local/csc/modulefiles                                                                                        
+module load pytorch                                                                                                           
+                                                                                                                              
+# This will store all the Hugging Face cache such as downloaded models                                                        
+# and datasets in the project's scratch folder                                                                                
+export HF_HOME=/scratch/${SLURM_JOB_ACCOUNT}/${USER}/hf-cache                                                                 
+mkdir -p $HF_HOME                                                                                                             
+                                                                                                                              
+srun python3 awq-modifier.py  
