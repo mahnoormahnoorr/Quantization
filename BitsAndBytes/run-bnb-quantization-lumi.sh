6ff7ed52df2a25ce4f2c22e8073f9b9a586ebf57
Anni Moisala <anni.moisala@csc.fi>
2025-09-23 09:05:24 +0300

    Update run-bnb-quantization-lumi.sh

--- BitsAndBytes/run-bnb-quantization-lumi.sh
+++ BitsAndBytes/run-bnb-quantization-lumi.sh
@@ -2,7 +2,6 @@
 #SBATCH --account=project_xxxxxxx 
 #SBATCH --partition=dev-g 
 #SBATCH --ntasks=1
-#SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=7
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=32G
